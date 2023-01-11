4dacde432ff8719e74aacfe9f7e0374221389eaa
chenyulab4126 <90142424+chenyulab4126@users.noreply.github.com>
2023-01-11 14:39:59 +0800

    Update step01_sle_cor.R

--- fig/step01_sle_cor.R
+++ fig/step01_sle_cor.R
@@ -9,7 +9,6 @@ library(gridExtra)
 library(FactoMineR)
 library(ggrepel)
 library(factoextra)
-#整理表达矩阵
 
 SLE_tpl_count <- read.table("data/SLE lung transplation/sle_counts.txt",
                             header = T,check.names = F)
@@ -22,7 +21,6 @@ colnames(sle_counts) <- c("Geneid","GeneName","CTRL1","CTRL2","SLE1","SLE2")
 save(sle_counts,file = "data/step01sle.Rdata")
 save(gene_info,file = "data/geneinfo.Rdata")
 
-#相关性分析
 cnt <- sle_counts[,c(3:ncol(sle_counts))]
 row.names(cnt) <- sle_counts$Geneid
 cnt_mat <- as.matrix(cnt)
@@ -33,7 +31,7 @@ write.csv(format(expr_cor,digits = 4),
           file.path(f_out,"expr_cor.csv"),
           row.names = T,
           quote = F)
-#相关性分析点图
+
 tmp_df1 <- data.frame(expr1 = cnt_mat[, 3], expr2 = cnt_mat[, 4])
 p1 <- ggplot(tmp_df1, aes(x = log10(expr1), y = log10(expr2))) +
   geom_point(size = 0.1, color = "black") +
